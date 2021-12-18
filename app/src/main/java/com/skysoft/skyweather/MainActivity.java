0c0b1ffdbebd7d34d4087aab4faa06e3660216b6
amarchenko700 <lexx700@yandex.ru>
2021-12-18 20:19:00 +0200

    Init commit

---
+++ app/src/main/java/com/skysoft/skyweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.skysoft.skyweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
