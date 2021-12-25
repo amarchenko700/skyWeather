e09a215e326f7758ddfce8791273fe30c5547792
amarchenko700 <lexx700@yandex.ru>
2021-12-25 23:18:06 +0200

    Проработал открытие карточки погоды

--- app/src/main/java/com/skysoft/skyweather/view/citieslist/viewmodel/ListCitiesViewModel.kt
+++ app/src/main/java/com/skysoft/skyweather/view/citieslist/viewmodel/ListCitiesViewModel.kt
@@ -5,29 +5,27 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.skysoft.skyweather.model.City
 import com.skysoft.skyweather.model.WeatherRepositoryImpl
-import com.skysoft.skyweather.view.citieslist.viewmodel.AppState
+import com.skysoft.skyweather.view.AppState
 import java.lang.Thread.sleep
 
 class ListCitiesViewModel(
     private val liveData: MutableLiveData<AppState> = MutableLiveData(),
-    private val repositoryImpl: WeatherRepositoryImpl = WeatherRepositoryImpl()
 ) : ViewModel() {
 
     fun getLiveData(): LiveData<AppState> {
         return liveData
     }
 
-    fun getWeatherFromServer() {
-        liveData.value = AppState.Loading(5)
-        Thread {
-            sleep(3000)
-            //liveData.postValue(AppState.Error(IllegalStateException("")))
-            val rand = (-10..20).random()
-            liveData.postValue(AppState.Success(repositoryImpl.getWeatherFromServer()))
-        }.start()
-    }
-
     fun openCityCard(city: City?) {
         liveData.value = AppState.openCityCard(city!!)
+    }
+
+    fun getCities() {
+        liveData.value = AppState.Loading(0)
+        Thread {
+            sleep(2000)
+            val rand = (-10..20).random()
+            liveData.postValue(AppState.SuccessLoadCities())
+        }.start()
     }
 }
---
+++ app/src/main/java/com/skysoft/skyweather/view/weathercard/WeatherFragment.kt
@@ -0,0 +1,68 @@
+package com.skysoft.skyweather.view.weathercard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import com.skysoft.skyweather.databinding.FragmentWeatherBinding
+import com.skysoft.skyweather.model.Weather
+import com.skysoft.skyweather.view.AppState
+
+class WeatherFragment(weather: Weather = Weather()) : Fragment() {
+
+    private lateinit var viewModel: WeatherViewModel
+    private var weather: Weather = weather
+    private var _binding: FragmentWeatherBinding? = null
+    private val binding: FragmentWeatherBinding
+        get() {
+            return _binding!!
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWeatherBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
+        viewModel.getLiveData().observe(viewLifecycleOwner, { renderData(it) })
+        viewModel.getWeatherFromServer()
+    }
+
+    private fun renderData(appState: AppState) {
+        when (appState) {
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(binding.mainViewWeather, "Error", Snackbar.LENGTH_LONG)
+                    .setAction("Попробовать еще раз") {
+                        viewModel.getWeatherFromServer()
+                    }.show()
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.SuccessLoadWeather -> {
+                binding.loadingLayout.visibility = View.GONE
+                fillCardWeather(weather)
+            }
+        }
+    }
+
+    fun fillCardWeather(weather: Weather) {
+        binding.cityName.text = weather.city.name
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
