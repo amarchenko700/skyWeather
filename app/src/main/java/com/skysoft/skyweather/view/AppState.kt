916fde2555a75a97fd42e175a6b8723d7dddddca
amarchenko700 <lexx700@yandex.ru>
2021-12-25 23:16:20 +0200

    Изменил класс с целью использования его и в списке городов, и для статуса загрузки карточки погоды

--- app/src/main/java/com/skysoft/skyweather/view/AppState.kt
+++ app/src/main/java/com/skysoft/skyweather/view/AppState.kt
@@ -1,11 +1,12 @@
-package com.skysoft.skyweather.view.citieslist.viewmodel
+package com.skysoft.skyweather.view
 
 import com.skysoft.skyweather.model.City
 import com.skysoft.skyweather.model.Weather
 
 sealed class AppState {
     data class Loading(val progress: Int) : AppState()
-    data class Success(val weatherData: Weather) : AppState()
+    class SuccessLoadCities() : AppState()
+    data class SuccessLoadWeather(val weatherData: Weather) : AppState()
     data class Error(val error:Throwable) : AppState()
     data class openCityCard(val city: City) : AppState()
 }
