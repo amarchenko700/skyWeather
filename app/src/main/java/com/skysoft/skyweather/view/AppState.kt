dc955c006916a1fcbb8fc45dedf1705b9378a76b
amarchenko700 <lexx700@yandex.ru>
2021-12-26 12:23:01 +0200

    Разделил класс, определяющий момент загрузки на 2: для погоды и списка городов. Для класса Error добавил второй параметр city - он нужен в случае, если пользователь хочет выполнить повторную загрузку

--- app/src/main/java/com/skysoft/skyweather/view/AppState.kt
+++ app/src/main/java/com/skysoft/skyweather/view/AppState.kt
@@ -4,9 +4,10 @@ import com.skysoft.skyweather.model.City
 import com.skysoft.skyweather.model.Weather
 
 sealed class AppState {
-    data class Loading(val progress: Int) : AppState()
+    data class LoadingCities(val progress: Int) : AppState()
+    data class LoadingWeather(val city: City) : AppState()
     class SuccessLoadCities() : AppState()
     data class SuccessLoadWeather(val weatherData: Weather) : AppState()
-    data class Error(val error:Throwable) : AppState()
+    data class Error(val error:Throwable, val city: City) : AppState()
     data class openCityCard(val city: City) : AppState()
 }
