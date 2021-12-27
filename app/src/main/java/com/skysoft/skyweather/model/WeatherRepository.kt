e92fb3545479605698f5852989d68d35420570c9
amarchenko700 <lexx700@yandex.ru>
2021-12-27 22:44:09 +0200

    Переписал репозитории на работу с двумя разными списками городов (российские и мировые)

--- app/src/main/java/com/skysoft/skyweather/model/WeatherRepository.kt
+++ app/src/main/java/com/skysoft/skyweather/model/WeatherRepository.kt
@@ -2,5 +2,6 @@ package com.skysoft.skyweather.model
 
 interface WeatherRepository {
     fun getWeatherFromServer(city: City): Weather
-    fun getWeatherFromLocalStorage(city: City): Weather
+    fun getWeatherFromLocalStorageRus(): List<Weather>
+    fun getWeatherFromLocalStorageWorld(): List<Weather>
 }
