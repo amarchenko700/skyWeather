1354ca76eb2794662a8a52bd01c7788d4457531d
amarchenko700 <lexx700@yandex.ru>
2021-12-27 22:48:18 +0200

    В связи с переработкой sealed class AppState внес необходимые изменения, также переписал на работу с методами получения разных списков городов

--- app/src/main/java/com/skysoft/skyweather/view/citieslist/ListCitiesViewModel.kt
+++ app/src/main/java/com/skysoft/skyweather/view/citieslist/ListCitiesViewModel.kt
@@ -3,28 +3,39 @@ package com.skysoft.skyweather.view.citieslist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.skysoft.skyweather.model.City
 import com.skysoft.skyweather.model.WeatherRepositoryImpl
 import com.skysoft.skyweather.view.AppState
 import java.lang.Thread.sleep
 
 class ListCitiesViewModel(
     private val liveData: MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: WeatherRepositoryImpl = WeatherRepositoryImpl()
+
 ) : ViewModel() {
 
     fun getLiveData(): LiveData<AppState> {
         return liveData
     }
 
-    fun openCityCard(city: City?) {
-        liveData.value = AppState.OpenCityCard(city!!)
-    }
-
-    fun getCities() {
-        liveData.value = AppState.LoadingCities(0)
+    fun getWeatherFromLocalServer(isRussian: Boolean) {
+        liveData.value = AppState.Loading(0)
         Thread {
             sleep(2000)
-            liveData.postValue(AppState.SuccessLoadCities)
+            liveData.postValue(
+                AppState.Success(
+                    if (isRussian) {
+                        repositoryImpl.getWeatherFromLocalStorageRus()
+                    } else {
+                        repositoryImpl.getWeatherFromLocalStorageWorld()
+                    }
+                )
+            )
         }.start()
     }
+
+    fun getWeatherFromLocalSourceRus() = getWeatherFromLocalServer(true)
+
+    fun getWeatherFromLocalSourceWorld() = getWeatherFromLocalServer(false)
+
+    fun getWeatherFromRemoteSource() = getWeatherFromLocalServer(true)
 }
