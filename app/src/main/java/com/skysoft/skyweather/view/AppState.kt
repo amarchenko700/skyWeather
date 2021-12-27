5f12b0477ede63668287136085fa89714d5d9a38
amarchenko700 <lexx700@yandex.ru>
2021-12-27 22:45:32 +0200

    Убрал лишние классы, остальные частично отредактировал

--- app/src/main/java/com/skysoft/skyweather/view/AppState.kt
+++ app/src/main/java/com/skysoft/skyweather/view/AppState.kt
@@ -4,10 +4,8 @@ import com.skysoft.skyweather.model.City
 import com.skysoft.skyweather.model.Weather
 
 sealed class AppState {
-    data class LoadingCities(val progress: Int) : AppState()
-    data class LoadingWeather(val city: City) : AppState()
-    object SuccessLoadCities : AppState()
+    data class Loading(val progress:Int) : AppState()
+    data class Success(val weatherData:List<Weather>): AppState()
     data class SuccessLoadWeather(val weatherData: Weather) : AppState()
     data class Error(val error:Throwable, val city: City) : AppState()
-    data class OpenCityCard(val city: City) : AppState()
 }
