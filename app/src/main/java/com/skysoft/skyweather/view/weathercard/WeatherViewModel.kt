a44009e2932909265fba942ee69edcfa90f59cef
amarchenko700 <lexx700@yandex.ru>
2022-01-12 23:27:58 +0200

    Попытался выполнить запрос на сервер

--- app/src/main/java/com/skysoft/skyweather/view/weathercard/WeatherViewModel.kt
+++ app/src/main/java/com/skysoft/skyweather/view/weathercard/WeatherViewModel.kt
@@ -3,14 +3,20 @@ package com.skysoft.skyweather.view.weathercard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
 import com.skysoft.skyweather.model.City
-import com.skysoft.skyweather.model.WeatherRepositoryImpl
+import com.skysoft.skyweather.model.Weather
 import com.skysoft.skyweather.view.AppState
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
 
 class WeatherViewModel(
     private val liveData: MutableLiveData<AppState> = MutableLiveData(),
-    private val repositoryImpl: WeatherRepositoryImpl = WeatherRepositoryImpl()
-) : ViewModel() {
+
+    ) : ViewModel() {
 
     fun getLiveData(): LiveData<AppState> {
         return liveData
@@ -18,23 +24,23 @@ class WeatherViewModel(
 
     fun getWeatherFromServer(city: City) {
         liveData.value = AppState.Loading(0)
-        Thread {
-            Thread.sleep(1500)
-            with(liveData) {
-                if (city.requestsCount >= 2) {
-                    postValue(
-                        AppState.SuccessLoadWeather(
-                            repositoryImpl.getWeatherFromServer(
-                                city
-                            )
-                        )
-                    )
-                } else {
-                    postValue(AppState.Error(IllegalStateException(""), city))
-                }
-            }
-            city.requestsCount++
-        }.start()
+//        Thread {
+        val url =
+            URL("https://api.weather.yandex.ru/v2/informers?lat=${city.latitude}&lon=${city.longitude}")
+        val urlConnection = (url.openConnection() as HttpsURLConnection).apply {
+            requestMethod = "GET"
+            readTimeout = 10000
+            addRequestProperty("Yandex-API-Key", "76597d0d-a647-418e-9e6e-d0f0a7be6d9c")
+        }
+
+        val reader = BufferedReader(InputStreamReader(urlConnection.inputStream))
+        val weather = Gson().fromJson(getLines(reader), Weather::class.java)
+        liveData.postValue(AppState.SuccessLoadWeather(weather))
+//        }.start()
+    }
+
+    private fun getLines(reader: BufferedReader): String {
+        return reader.lines().collect(Collectors.joining("\n"))
     }
 
     fun getWeather(city: City) {
