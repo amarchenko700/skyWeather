bb389865426d3a06b66a5b2f3d50ef250ddb98d6
amarchenko700 <lexx700@yandex.ru>
2022-01-28 15:12:28 +0200

    Убрал упоминание контекста и добавил метод getConvertedWeatherFromWeatherDTO

--- app/src/main/java/com/skysoft/skyweather/repository/RepositoryWeather.kt
+++ app/src/main/java/com/skysoft/skyweather/repository/RepositoryWeather.kt
@@ -1,11 +1,12 @@
 package com.skysoft.skyweather.repository
 
-import android.content.Context
 import com.skysoft.skyweather.model.City
+import com.skysoft.skyweather.model.Weather
 import com.skysoft.skyweather.model.WeatherDTO
 import retrofit2.Callback
 
 interface RepositoryWeather {
     fun getWeatherFromServer(city: City, callback: Callback<WeatherDTO>)
-    fun getWeather(city: City, context: Context)
+    fun getWeather(city: City)
+    fun getConvertedWeatherFromWeatherDTO(weatherDTO: WeatherDTO, city: City): Weather
 }
